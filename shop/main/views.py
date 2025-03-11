4fbf0cd4cb4f9e3baafac7a31b3b7cff5b12b7b2
Rustamov <rustamovasilbek1221@gmail.com>
2025-03-11 06:29:28 +0500

    first commit

--- shop/main/views.py
+++ shop/main/views.py
@@ -1,6 +1,24 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+from .models import Product, Category, ProductImage
 
-# Create your views here.
 
-def index(request):
-    return render(request, 'index.html')
+
+
+class IndexView(ListView):
+    queryset = Product.objects.all()[:10]
+    template_name = "index.html"
+    context_object_name = 'products'
+
+
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data()
+        context['categories'] = Category.objects.filter(parent=None)
+        images = []
+        for image in ProductImage.objects.all():
+            images.append(image.image.url)
+        context['images'] = images
+        return context
+
+
--- shop/shop/settings.py
+++ shop/shop/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 
+from django.conf.global_settings import STATICFILES_DIRS
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -119,6 +121,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT=BASE_DIR / 'static'
+STATICFILES_DIRS=[
+    BASE_DIR/'staticfiles'
+]
 
 MEDIA_URL = 'media/'
 MEDIA_ROOT = BASE_DIR / 'media'
